f46027e2525a24cbcaefc3e50bc2a04f0ffd1ca8
CodingFactoryT <tim.baesler@gmail.com>
2024-12-13 22:59:08 +0100

    added solution for day 13 part 2

--- 2024/src/Day13/Part2.java
+++ 2024/src/Day13/Part2.java
@@ -1,3 +1,4 @@
+
 package Day13;
 
 import java.nio.file.Files;
@@ -6,16 +7,18 @@ import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class Part1 {
+public class Part2 {
     public static void main(String[] args) throws Exception {
         List<String> input  = Files.readAllLines(Path.of("./2024/src/Day13/input.txt"));
         double[][] solvedGauss = parseAndSolveGauss(input);
 
         long sum = 0;
+        int i = 0;
         for(double[] l : solvedGauss) {
-            if(l[0] > 0 && l[0] <= 100 && isEffectivelyInteger(l[0]) && l[1] > 0 && l[1] <= 100 && isEffectivelyInteger(l[1])) {
+            if(l[0] > 0 && isEffectivelyLong(l[0]) && l[1] > 0 && isEffectivelyLong(l[1])) {
                 sum += 3 * l[0] + l[1];
             }
+            i++;
         }
         System.out.println(sum);
     }
@@ -23,7 +26,7 @@ public class Part1 {
     private static double[][] parseAndSolveGauss(List<String> input) throws Exception {
         Iterator<String> inputIterator = input.iterator();
         double[][] solvedGauss = new double[input.size() / 4 + 1][2];
-        ArrayList<Integer> localGaussList = new ArrayList<>();
+        ArrayList<Long> localGaussList = new ArrayList<>();
         Pattern pattern = Pattern.compile("X[+=](\\d+), Y[+=](\\d+)");
         int index = 0;
         while(inputIterator.hasNext()) {
@@ -42,8 +45,12 @@ public class Part1 {
             for(int i = 0; i < 3; i++) {
                 Matcher matcher = pattern.matcher(currentLine);
                 if(matcher.find()) {
-                    int firstNum = Integer.parseInt(matcher.group(1));
-                    int secondNum = Integer.parseInt(matcher.group(2));
+                    long firstNum = Integer.parseInt(matcher.group(1));
+                    long secondNum = Integer.parseInt(matcher.group(2));
+                    if(i == 2) {
+                        firstNum += 10000000000000L;
+                        secondNum += 10000000000000L;
+                    }
                     localGaussList.add(firstNum);
                     localGaussList.add(secondNum);
                 } else {
@@ -55,11 +62,18 @@ public class Part1 {
             }
         }
 
+        double[][] A = new double[][]{{localGaussList.get(0), localGaussList.get(2)}, {localGaussList.get(1), localGaussList.get(3)}};
+        double[] x = new double[]{localGaussList.get(4), localGaussList.get(5)};
+        double[] gaussSolution = GaussianElimination.lsolve(A, x);
+        solvedGauss[index][0] = gaussSolution[0];
+        solvedGauss[index][1] = gaussSolution[1];
+
         return solvedGauss;
     }
 
-    private static boolean isEffectivelyInteger(double number) {
-        double tolerance = 1e-6;
+    private static boolean isEffectivelyLong(double number) {
+        double tolerance = 1e-2;
         return Math.abs(number - Math.round(number)) < tolerance;
     }
+
 }
